a6c8df966182013ebc0d53ed8c384e79d9d542e1
Yusuf <akuyusuf27@gmail.com>
2020-07-18 14:48:10 +0700

    refactor: 💡 check required parameter

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,8 @@
  * @param {string | number} width
  */
 export function getImage(path, width = "original") {
+  if (!path) return undefined
+
   const size = typeof width === "number" ? `w${width}` : width
   return `https://image.tmdb.org/t/p/${size}${path}`
 }
@@ -14,6 +16,8 @@ export function getImage(path, width = "original") {
  * @returns {string} with format date: MMM, d YYYY
  */
 export function formatDate(date) {
+  if (!date) return undefined
+
   return new Date(date).toLocaleDateString("en-US", {
     day: "numeric",
     month: "short",
