e9776f2140d85d3dc6ea6af606e8cc8f612b2503
Yusuf <akuyusuf27@gmail.com>
2020-07-18 14:46:52 +0700

    refactor: 💡 function to call http request

--- src/services/movie.js
+++ src/services/movie.js
@@ -2,18 +2,23 @@ import axios from "axios"
 import { useQuery } from "react-query"
 import { API_KEY, BASE_URL } from "../constants/api"
 
-const http = axios.create({
-  baseURL: BASE_URL,
-  params: {
-    api_key: API_KEY,
-  },
-})
-
-export function useNowPlaying() {
-  const getAllPopular = async () => {
-    const { data } = await http.get("/movie/now_playing")
-    return data
+const fetcher = async (endpoint, params = {}) => {
+  if (!endpoint) {
+    throw new Error('fetcher require argument "endpoint"')
   }
 
-  return useQuery("now_playing", getAllPopular)
+  const options = {
+    baseURL: BASE_URL,
+    params: {
+      api_key: API_KEY,
+      ...params,
+    },
+  }
+
+  const { data } = await axios.get(endpoint, options)
+  return data
+}
+
+export function useNowPlaying() {
+  return useQuery("now_playing", () => fetcher(`/movie/now_playing`))
 }
